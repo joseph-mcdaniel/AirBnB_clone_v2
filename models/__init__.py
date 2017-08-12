6bf9b472b1f57dcc82df775f65b519d47c9d37d8
spacexengineer <hunter.casbeer@gmail.com>
2017-08-12 16:15:42 -0700

    init update for condition on HBNB_TYPE_STORAGE if equal to db (reference to DBStorage class)

--- models/__init__.py
+++ models/__init__.py
@@ -6,8 +6,15 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from models.engine import db_storage
+from os import getenv
 
 storage = file_storage.FileStorage()
-storage.reload()
+# storage.reload()
 """CNC - dictionary = { Class Name (string) : Class Type }"""
 CNC = file_storage.FileStorage.CNC
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    storage = db_storage.DBStorage()
+
+storage.reload()
