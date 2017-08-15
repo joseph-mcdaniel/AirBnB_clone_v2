e3d5e5a5c92abbd65c82da1fe7221bf2eca48f69
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-08-15 23:26:50 +0000

    add attributes for db and engine check

--- models/state.py
+++ models/state.py
@@ -4,12 +4,19 @@ State Class from Models Module
 """
 
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models import *
 
 class State(BaseModel):
     """State class handles all application states"""
-
-    name = ''
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                          cascade="all, delete, delete-orphan")
+    else:
+        name = ''
 
     def __init__(self, *args, **kwargs):
         """instantiates a new state"""
