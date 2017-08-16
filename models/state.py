9c0170e0f48be06295fc37c5966b8962678c5e66
spacexengineer <hunter.casbeer@gmail.com>
2017-08-15 18:05:23 -0700

    Add Base inheritance after BaseModel

--- models/state.py
+++ models/state.py
@@ -10,7 +10,7 @@ from models import *
 from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """State class handles all application states"""
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = "states"
