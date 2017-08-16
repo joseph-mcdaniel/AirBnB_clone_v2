83dff8ae68d58a6d74b3289d080e0f9184f5b8ea
spacexengineer <hunter.casbeer@gmail.com>
2017-08-16 16:00:24 -0700

    remove delete in cascade

--- models/state.py
+++ models/state.py
@@ -16,7 +16,7 @@ class State(BaseModel, Base):
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state",
-                              cascade="all, delete, delete-orphan")
+                              cascade="all, delete-orphan")
     else:
         name = ''
 
