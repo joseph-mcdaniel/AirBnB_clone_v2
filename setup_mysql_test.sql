393b44537852b1890036b889d9c8eca0ab93a147
spacexengineer <hunter.casbeer@gmail.com>
2017-08-16 14:54:20 -0700

    remove first flush

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,5 @@
 -- prepares mysql server
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-FLUSH PRIVILEGES;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
 				IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
