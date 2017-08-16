67c547d4bd5cea29162d62e0d3d0764480ac9693
spacexengineer <hunter.casbeer@gmail.com>
2017-08-16 14:57:48 -0700

    fix typos in environment vars naming

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,6 +3,7 @@
 database storage
 """
 from models import base_model, amenity, city, place, review, state, user
+from models.base_model import Base
 from os import environ, getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -12,19 +13,20 @@ class DBStorage:
     __engine = None
     __session = None
 
-    def init(self):
+    def __init__(self):
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(getenv('HBNB_MYQL_USER'),
+                                      .format(getenv('HBNB_MYSQL_USER'),
                                               getenv('HBNB_MYSQL_PWD'),
                                               getenv('HBNB_MYSQL_HOST'),
                                               getenv('HBNB_MYSQL_DB')))
+        """
         self.__models = {'User': User,
                          'Amenity': Amenity,
                          'City': City,
                          'Place': Place,
                          'Review': Review,
                          'State': State}
-
+        """
         if getenv('HBNB_ENV') == "test":
             Base.metadata.drop_all(self.__engine)
 
@@ -36,13 +38,15 @@ class DBStorage:
         return obj
 
     def new(self, obj):
-        self.__session.add(obj)
+        if obj:
+            self.__session.add(obj)
 
     def save(self):
         self.__session.commit()
 
     def delete(self, obj=None):
-        self.__session.remove()
+        if obj:
+            self.__session.remove()
 
     def reload(self):
         Base.metadata.create_all(self.__engine)
