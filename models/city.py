a0e69f980af794c56f3b20915ee049ce845e5cd4
spacexengineer <hunter.casbeer@gmail.com>
2017-08-16 21:23:42 -0700

    remove unecessary

--- models/city.py
+++ models/city.py
@@ -16,8 +16,6 @@ class City(BaseModel, Base):
         __tablename__ = "cities"
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-#        cities = relationship("Place", backref="cities",
-#                              cascade="all, delete-orphan")
         places = relationship('Place', backref='cities',
                               cascade='all, delete-orphan')
     else:
