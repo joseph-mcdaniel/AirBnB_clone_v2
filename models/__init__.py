13f07e73c6b40931eb5bb39736792614eadaa51b
spacexengineer <hunter.casbeer@gmail.com>
2017-08-16 20:37:07 -0700

    add reference to CNC from db_storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
-from models.engine import file_storage
+#!/usr/bin/python3
+from os import environ
 from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.city import City
@@ -6,15 +7,14 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from os import getenv
 
-
-if getenv("HBNB_TYPE_STORAGE") == "db":
+"""conditional to determine the storage engine type"""
+if "HBNB_TYPE_STORAGE" in environ and environ["HBNB_TYPE_STORAGE"] == 'db':
     from models.engine import db_storage
     storage = db_storage.DBStorage()
+    CNC = db_storage.DBStorage.CNC
 else:
+    from models.engine import file_storage
     storage = file_storage.FileStorage()
+    CNC = file_storage.FileStorage.CNC
 storage.reload()
-
-"""CNC - dictionary = { Class Name (string) : Class Type }"""
-CNC = file_storage.FileStorage.CNC
