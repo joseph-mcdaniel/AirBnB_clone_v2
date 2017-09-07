1c5c0279eb7b91211361aae3fb93f5f8e6439806
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-09-07 01:57:05 +0000

    initial commit

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
