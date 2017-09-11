0303ef617e21bf7b573863b9c21ed8c2383fd93d
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-09-11 03:02:49 +0000

    add public getter method

--- models/state.py
+++ models/state.py
@@ -6,8 +6,8 @@ State Class from Models Module
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models import *
 from os import getenv
+import models
 
 
 class State(BaseModel, Base):
@@ -23,3 +23,11 @@ class State(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """instantiates a new state"""
         super().__init__(self, *args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """return the list of City objects linked to the current State"""
+            cities = models.storage.all('City').values()
+            current = [obj for obj in cities if obj.state_id == self.id]
+            return (current)
